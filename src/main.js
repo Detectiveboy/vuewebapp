ed32777b600edc386df91e2120546ed68b542f6e
Detectiveboy <m16639649502@163.com>
2020-04-09 18:13:22 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import NutUI from '@nutui/nutui';
+import '@nutui/nutui/dist/nutui.css';
 
+NutUI.install(Vue);
 Vue.config.productionTip = false
 
 new Vue({
